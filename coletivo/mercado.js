422eb7675cf6ad7c654d62e018341793a6cd5f95
lucasprezom <lucasprezom@gmail.com>
2020-08-18 22:04:59 -0300

    cards dinamicos com a api e fotos

--- coletivo/mercado.js
+++ coletivo/mercado.js
@@ -1,79 +1,83 @@
-// const rs = require("readline-sync")
-
 // const axios = require('axios')
 
-function carregarProdutos() {
+
+window.onload = function() {
+  carregarProdutos()
+} 
+async function carregarProdutos() {
     let divProdutos = document.getElementById("todosOsProdutos")
-    axios.get('http://localhost:3000/Produtos')
+    let produtos
+  
+    await axios.get('http://localhost:3000/Produtos')
     .then(res => {
-        let produtos = res.data
+        produtos = res.data
+        
     })
-    
-    for (let i = 0; i <= produtos.length; i++) {
+
+    for (let i = 0; i < produtos.length; i++) {
         let produtoAtual = document.createElement("div")
-      produtoAtual.innerHTML = `
-        <div class="row">
-      <div class="column">
+      produtoAtual.innerHTML =
+       `<div class="column">
         <div class="card">
-          <img src="${produtos.foto}" alt="${produtos.nome}">
-          <input class="quantidade">
-          <h2>${produtos.preco}</h2>
-          <button id="comprar${produtos.nome}" type="button" class="btn btn-outline-success">Comprar</button>
+          <img src="${produtos[i].foto}" alt="${produtos[i].nome}">
+          <h5>${produtos[i].nome}</h5>         
+          <input class="quantidade" type="number">
+          <h4>R$ ${produtos[i].preco}0</h4>
+          <button id="comprar${produtos[i].nome}" type="button" class="btn btn-outline-success">Comprar</button>
         </div>
       </div>`
         divProdutos.appendChild(produtoAtual)
-    }
-}
-carregarProdutos()
+    } 
+} 
 
-let carrinho = []
+// let carrinho = []
 
-class produtoNoCarrinho {
-    constructor(nome, valor, quantidade) {
-      this.nome = nome
-      this.valor = valor
-      this.quantidade = quantidade
-    }
-  }
+// class produtoNoCarrinho {
+//     constructor(nome, valor, quantidade) {
+//       this.nome = nome
+//       this.valor = valor
+//       this.quantidade = quantidade
+//     }
+//   }
 
-  function colocarProdutoNoCarrinho(nome, valor, quantidade) {
-    if (quantidade !== "") {
-      let produtoNoCarrinho = new ProdutoNoCarrinho(nome, valor, quantidade)
-      carrinho.push(produtoNoCarrinho)
-      console.log("Coloquei " + produtoNoCarrinho.quantidade + " " + produtoNoCarrinho.nome + "(s) no carrinho!")
-      calcularValorTotalDoCarrinho()
-    } else {
-      alert("Voce esqueceu de selecionar a quantidade desejada.")
-    }
-  }
+//   function colocarProdutoNoCarrinho(nome, valor, quantidade) {
+//     if (quantidade !== "") {
+//       let produtoNoCarrinho = new ProdutoNoCarrinho(nome, valor, quantidade)
+//       carrinho.push(produtoNoCarrinho)
+//       console.log("Coloquei " + produtoNoCarrinho.quantidade + " " + produtoNoCarrinho.nome + "(s) no carrinho!")
+//       calcularValorTotalDoCarrinho()
+//     } else {
+//       alert("Voce esqueceu de selecionar a quantidade desejada.")
+//     }
+//   }
 
-  colocarProdutoNoCarrinho()
+//   colocarProdutoNoCarrinho()
 
-  window.onload = function() {
-    document.getElementById("comprarAlface").addEventListener("click", () => {
+//   window.onload = function() {
+//     document.getElementById("comprarAlface").addEventListener("click", () => {
 
-        clicarComprarProduto("precoAlface", "quantidadeAlface", "Alface")
+//         clicarComprarProduto("precoAlface", "quantidadeAlface", "Alface")
     
-      })
-      document.getElementById("comprarUva").addEventListener("click", () => {
+//       })
+//       document.getElementById("comprarUva").addEventListener("click", () => {
     
-        let idDoCampoPreco = "precoUva"
-        let idDoCampoQuantidade = "quantidadeUva"
-        let nomeDoProduto = "Uva"
+//         let idDoCampoPreco = "precoUva"
+//         let idDoCampoQuantidade = "quantidadeUva"
+//         let nomeDoProduto = "Uva"
     
-        clicarComprarProduto(idDoCampoPreco, idDoCampoQuantidade, nomeDoProduto)
+//         clicarComprarProduto(idDoCampoPreco, idDoCampoQuantidade, nomeDoProduto)
     
-      })
-      document.getElementById("comprarUvaSemCaroco").addEventListener("click", () => {
+//       })
+//       document.getElementById("comprarUvaSemCaroco").addEventListener("click", () => {
     
-        let idDoCampoPreco = "precoUvaSemCaroco"
-        let idDoCampoQuantidade = "quantidadeUvaSemCaroco"
-        let nomeDoProduto = "Uva Sem Caroço"
-        clicarComprarProduto(idDoCampoPreco, idDoCampoQuantidade, nomeDoProduto)
-      })
-      document.getElementById("comprarPera").addEventListener("click", () => {
+//         let idDoCampoPreco = "precoUvaSemCaroco"
+//         let idDoCampoQuantidade = "quantidadeUvaSemCaroco"
+//         let nomeDoProduto = "Uva Sem Caroço"
+//         clicarComprarProduto(idDoCampoPreco, idDoCampoQuantidade, nomeDoProduto)
+//       })
+//       document.getElementById("comprarPera").addEventListener("click", () => {
     
-        clicarComprarProduto("precoPera", "quantidadePera", "Pera")
+//         clicarComprarProduto("precoPera", "quantidadePera", "Pera")
         
-      })
-  }
+//       })
+//   }
